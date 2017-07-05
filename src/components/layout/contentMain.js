a6be7da12cc2bec72546ccdc9b0fc37166db9378
muraguri64 <muraguri64@gmail.com>
2017-07-05 20:12:37 +0300

    Add Login Form

--- src/components/layout/contentMain.js
+++ src/components/layout/contentMain.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { 
-      Content, Text
+      Content, Form, Item, Input, Label, Button, Text
  } from 'native-base';
+
+ import { View } from 'react-native';
 
  class Contentmain extends Component {      
     render() {
         return (
 
-            <Content padder>                
-                <Text>Login Form area</Text>                                 
+            <Content padder>                 
+                              
+                <Form>
+                    <Item floatingLabel>
+                        <Label>Username</Label>
+                        <Input />
+                    </Item>
+                    <Item floatingLabel>
+                        <Label>Password</Label>
+                        <Input />
+                    </Item>
+                </Form>
+                <View style={{ marginTop: 40 }}>
+                    <Button block rounded>
+                        <Text>Sign In</Text>
+                    </Button>
+                </View>                                
             </Content>
         ); 
     }
