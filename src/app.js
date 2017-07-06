23bd7bd9425dc04c9b5cab8bd6e1169b931a5d43
muraguri64 <muraguri64@gmail.com>
2017-07-06 22:11:24 +0300

    Initialize firebase

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { Container } from 'native-base';
 
+//import firebase
+import firebase from 'firebase';
+
 //import custom components - header and content
 import HeaderTop from './components/layout/headerTop';
 import ContentMain from './components/layout/contentMain';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({           
+            apiKey: 'AIzaSyCx5haiOdUoChn9Az-NbJ-DNAYLvVkoHF0',
+            authDomain: 'authentication-f1b5d.firebaseapp.com',
+            databaseURL: 'https://authentication-f1b5d.firebaseio.com',
+            projectId: 'authentication-f1b5d',
+            storageBucket: 'authentication-f1b5d.appspot.com',
+            messagingSenderId: '833386039807'
+        });
+    }
     
     render() {
         return (
