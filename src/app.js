57000ff55c775d62c58bd782df1038280809487b
muraguri64 <muraguri64@gmail.com>
2017-07-08 21:44:06 +0300

    Handling authentication event

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import HeaderTop from './components/layout/headerTop';
 import ContentMain from './components/layout/contentMain';
 
 class App extends Component {
+    state = { loggedIn: false };
     componentWillMount() {
         firebase.initializeApp({           
             apiKey: 'AIzaSyCx5haiOdUoChn9Az-NbJ-DNAYLvVkoHF0',
@@ -17,6 +18,14 @@ class App extends Component {
             projectId: 'authentication-f1b5d',
             storageBucket: 'authentication-f1b5d.appspot.com',
             messagingSenderId: '833386039807'
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
         });
     }
     
